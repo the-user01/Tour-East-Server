a50dd7f1596d588cf32c306ba49bac6ccb68d931
the-user01 <sakibcom02@gmail.com>
2024-04-28 01:24:50 +0600

    Updated codes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
@@ -35,6 +35,14 @@ async function run() {
 
             const result =  await cursor.toArray()
             
+            res.send(result);
+
+        })
+
+        app.get('/allSpots/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await spotCollection.findOne(query);
             res.send(result);
 
         })
