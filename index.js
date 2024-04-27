6cb3b9f07c7126c9ec53d810d2d957159ef7a01d
the-user01 <sakibcom02@gmail.com>
2024-04-27 17:31:07 +0600

    updated gitignore and env file

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// tourism-manager
-// svBSrWaOzbHCaMM7
 
-const uri = "mongodb+srv://tourism-manager:svBSrWaOzbHCaMM7@cluster0.8yiviav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8yiviav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
